ae30ae11291ce4c4f3510e70ac5f94776cc91f20
mariusortiz <122994686+mariusortiz@users.noreply.github.com>
2024-10-04 16:34:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -212,6 +212,8 @@ if selected_coin == "Alephium":
         total_aleph_24_months += monthly_reward
         rewards.append(monthly_reward)
 
+ st.write(f"Récompenses projetées sur 24 mois : **{total_aleph_24_months:,.2f} ALEPH**")
+
 elif selected_coin == "KAS":
     # Liste complète des données d'émission de KAS (comme avant)
     data = {
